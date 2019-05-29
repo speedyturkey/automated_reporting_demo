ee8f4399f86077669af0503982ee28b46489632a
Billy McMonagle <speedyturkey@gmail.com>
2019-05-28 20:12:13 -0400

    Sample Report 3 - Use GenericReport instead

--- reports/sample_report_3.py
+++ reports/sample_report_3.py
@@ -1,8 +1,9 @@
-from porthole import BasicReport, QueryExecutor
+from porthole import GenericReport, QueryExecutor
 
 
 def main():
-    report = BasicReport(
+    report = GenericReport(
+        report_name='sample_report',
         report_title="US Geography - Top Cities"
     )
     report.build_file()
@@ -17,12 +18,7 @@ def main():
     )
     top_city_name = result.result_data[0]["City Name"]
     top_city_pop = result.result_data[0]["Est. Population (2017)"]
-    report.to_recipients = ['william.mcmonagle@ankura.com']
-    report.cc_recipients = [
-        'edward.nunes@ankura.com',
-        'shane.metzger@ankura.com',
-        'chase.hudson@ankura.com'
-    ]
+    report.get_recipients()
     report.subject = "Sample Report 3: US Geography - Top Cities by Population"
     report.message = f"""Attached is a list of US cities with populations of more than 500,000. There
 are { result.result_count } cities meeting this criteria. The top city is
